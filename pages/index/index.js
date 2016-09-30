c1becca231be2801be1133cbb9b02f5441418728
big67 <1020431814@qq.com>
2016-09-30 16:59:32 +0800

    weibo

--- pages/index/index.js
+++ pages/index/index.js
@@ -28,10 +28,21 @@ Page({
                 avatar_url:'../../image/addison.png',
                 loginname:'Master Chen'
             },
+            postImages:[
+                {
+                    url:"http://img2.fengniao.com/product/157/787/cebMfDNyq6ic.jpg"
+                },
+                {
+                    url:"http://img2.fengniao.com/product/157/785/ce17Rr8Ravz2k.jpg"
+                },
+                {
+                    url:"http://img2.fengniao.com/product/157/784/ceQMH7d1RpFXs.jpg"
+                }
+            ],
             top:false,
             good:false,
             last_reply_at:'1小时前',
-            title:'Let\'s take some photos',
+            title:'昨晚拍得美女，你们喜欢吗？',
             reply_count:'10',
             visit_count:'100'
         },
@@ -41,14 +52,25 @@ Page({
                 avatar_url:'../../image/Lee.png',
                 loginname:'Master Lee'
             },
+            postImages:[
+                {
+                    url:"http://cms.fn.img-space.com/t_s700x2000/g2/M00/04/84/Cg-4klbwr2qIXSg5AIi2SZ-25BsAAHCCgK7iSMAiLZh596.jpg"
+                },
+                {
+                    url:"http://cms.fn.img-space.com/t_s700x2000/g1/M00/06/BF/Cg-4q1bwr26ITEpzAEKp8RdWF_kAAQnAgAiIHgAQqoJ874.jpg"
+                },
+                {
+                    url:"http://cms.fn.img-space.com/t_s700x2000/g1/M00/06/BF/Cg-4rFbwr8OIZI-ZAFhhqv27bRsAAQnAwJcGx4AWGHC949.jpg"
+                }
+            ],
             top:false,
             good:false,
             last_reply_at:'2小时前',
-            title:'I\'m master of light',
+            title:'窈窕淑女，尽收镜底',
             reply_count:'5',
             visit_count:'150'
         },
-                {
+        {
             id:'2',
             author:{   
                 avatar_url:'../../image/addison.png',
